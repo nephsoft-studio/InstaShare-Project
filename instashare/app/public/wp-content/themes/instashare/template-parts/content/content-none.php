5b04c99da36bd876e180a569e6131f040dc0798d
nephsoft-studio <noctestudio96@gmail.com>
2023-10-06 17:49:31 -0400

    content inclusion

--- instashare/app/public/wp-content/themes/instashare/template-parts/content/content-none.php
+++ instashare/app/public/wp-content/themes/instashare/template-parts/content/content-none.php
@@ -8,3 +8,14 @@
  */
 
 ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('blog-items'); ?>>
+	<div class="blog-wrapup">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+			<p class="pt-5"><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'instashare' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+		<?php elseif ( is_search() ) : ?>
+			<p class="pt-5"><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'instashare' ); ?></p>
+		<?php else : ?>
+			<p class="pt-5"><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'instashare' ); ?></p>
+		<?php endif; ?>
+	</div>
+</article>
