e4207f7b8006506145781c6ef5b90d1e76d993c2
nephsoft-studio <noctestudio96@gmail.com>
2023-10-06 19:00:59 -0400

    templates inclusion

--- instashare/app/public/wp-content/themes/instashare/templates/template-homepage.php
+++ instashare/app/public/wp-content/themes/instashare/templates/template-homepage.php
@@ -1,5 +1,13 @@
 <?php 
 /**
 Template Name: Homepage
-*/ 
+*/
+
+get_header(); 
 ?>
+
+<?php
+	do_action( 'instashare_sections', false );
+	get_template_part('template-parts/sections/section','blog'); 
+		
+get_footer(); ?>
