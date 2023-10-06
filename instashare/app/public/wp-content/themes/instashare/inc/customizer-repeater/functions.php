96383417cb4f5a88e423d23b501b1673a42c337b
nephsoft-studio <noctestudio96@gmail.com>
2023-10-06 16:46:47 -0400

    customizer repeater inclusion

--- instashare/app/public/wp-content/themes/instashare/inc/customizer-repeater/functions.php
+++ instashare/app/public/wp-content/themes/instashare/inc/customizer-repeater/functions.php
@@ -3,3 +3,7 @@
  * @package Instashare
  */
 ?>
+<?php
+define( 'Instashare_CUSTOMIZER_REPEATER_VERSION', '1.1.0' );
+
+require get_template_directory() . '/inc/customizer-repeater/inc/customizer.php';
